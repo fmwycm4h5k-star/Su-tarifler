1de6ff135be63cf8efc3ebc875f5656783f8ecc3
fmwycm4h5k-star <fmwycm4h5k@privaterelay.appleid.com>
2026-08-13 23:55:51 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,6 @@
 const SUPABASE_URL = "https://zdgdbnmhaagurcmvjolk.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = sb_publishable_e3LL6Q-SxYhWnHEBjpcgNw_fBT30pkZ
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
